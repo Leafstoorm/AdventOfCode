07dba07c040b9798f1f21b33d475ebf53c3c1422
Rafal <rafael.galinski666@gmail.com>
2025-12-02 16:41:41 +0100

    test

---
+++ Advent20241.java
@@ -0,0 +1,65 @@
+package AdventOfCode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Advent20241 {
+    private static String path = "C:\\Users\\user\\OneDrive\\PropCprog\\AdventOfCode\\";
+    public static void main(String[] args) {
+        PriorityQueue<Integer> list1 = new PriorityQueue<>();
+        PriorityQueue<Integer> list2 = new PriorityQueue<>();
+        populateLists(list1, list2);
+        System.out.println("total distance: " + getTotalDistance(list1, list2));
+        System.out.println("Similarity score: " + getSimilarityScore(list1, list2));
+        
+    }
+    
+    public static int getTotalDistance(PriorityQueue<Integer> list1, PriorityQueue<Integer> list2) {
+        PriorityQueue<Integer> list1copy = new PriorityQueue<>(list1);
+        PriorityQueue<Integer> list2copy = new PriorityQueue<>(list2);
+
+        int size = list1copy.size();
+        int totalDistance = 0;
+        for (int i = 0; i < size; i++) {
+            totalDistance += Math.abs(list1copy.poll() - list2copy.poll());
+        }
+        return totalDistance;
+    }
+
+    public static int getSimilarityScore(PriorityQueue<Integer> list1, PriorityQueue<Integer> list2) {
+        PriorityQueue<Integer> list1copy = new PriorityQueue<>(list1);
+        PriorityQueue<Integer> list2copy = new PriorityQueue<>(list2);
+
+        int size = list1copy.size();
+        int totalSimilarity = 0;
+        for (int i = 0; i < size; i++) {
+            int num = list1copy.poll();
+            int count = 0;
+            for (Integer n : list2copy) {
+                if (n == num) {
+                    count++;
+                }
+            }
+            totalSimilarity += num * count;
+        }
+        return totalSimilarity;
+    }
+
+    public static void populateLists(PriorityQueue<Integer> list1, PriorityQueue<Integer> list2) {
+        try (Scanner sc = new Scanner(new File(path + "input.txt"))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+
+                // Split on tab or multiple spaces
+                String[] parts = line.split("\\s+");
+
+                list1.add(Integer.parseInt(parts[0])); 
+                list2.add(Integer.parseInt(parts[1]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: input.txt not found!");
+        }
+    }
+}
---
+++ Advent20252.java
@@ -0,0 +1,55 @@
+package AdventOfCode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Advent20252 {
+    private static String path = "C:\\Users\\user\\OneDrive\\PropCprog\\AdventOfCode\\";
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        try (Scanner sc = new Scanner(new File(path + "input2.txt"))) {
+            long result = 0;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+
+
+                // Split on commas
+                String[] ranges = line.split(",");
+                //for each range (got all numbers)
+                for (String r : ranges) {
+                    String[] nums = r.split("-");
+                    long start = Long.parseLong(nums[0]);
+                    long end = Long.parseLong(nums[1]);
+                    //for each number
+                    for (long i = start; i <= end; i++) {
+                        String s = Long.toString(i);
+                        //check for each division
+                        for (int j = 2; j <= s.length(); j++) {
+                            //if divisible
+                            if (s.length() % j == 0) {
+                                int chunkLen = s.length() / j;
+                                int k = 1;
+                                while (k < j && s.regionMatches(0, s, k * chunkLen, chunkLen)) {
+                                    k++;
+                                }
+                                if (k == j) {
+                                    //System.out.println(i + " is made of " + j + " repeats");
+                                    result += i;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } 
+                System.out.println(result);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: input.txt not found!");
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Took " + (endTime - startTime) + " ms");  
+    }
+}
